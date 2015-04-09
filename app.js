3302e81a130103818c2fe80d152835c91c500fb8
Pablo F. Mescher <pfmescher@gmail.com>
2015-04-09 18:06:41 -0300

    Working on client

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-var socket = require("socket.io");
-var http = require("http");
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var config = require('./config.json');
+var path = require("path");
 
 
+server.listen(8080);
+
+app.use(express.static(path.join(__dirname, "bower_components")));
+app.use(express.static(path.join(__dirname, "static")));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
