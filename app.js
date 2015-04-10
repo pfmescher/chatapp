8d41e285a6355681613a52eaf80c5a48f7c22ec3
Pablo Mescher <pfmescher@gmail.com>
2015-04-10 13:55:18 -0300

    Basic chat functionality in place

--- app.js
+++ app.js
@@ -4,20 +4,44 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var config = require('./config.json');
 var path = require("path");
+var clients = {};
 
+function Message(message, owner, nickname) {
+    this.message = message;
+    this.owner = owner;
+    this.nickname = nickname;
+}
+
+Message.prototype.toJson = function () {
+    return JSON.stringify(this);
+};
 
 server.listen(8080);
 
-app.use(express.static(path.join(__dirname, "bower_components")));
-app.use(express.static(path.join(__dirname, "static")));
+app.use("/bower", express.static(path.join(__dirname, "bower_components")));
+app.use("/static", express.static(path.join(__dirname, "static")));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
 
 io.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-        console.log(data);
+    console.log("A user connected");
+    socket.emit("chat message", (new Message("You are currently on the main room", "system", "System")));
+
+    socket.emit("chat message",
+        (new Message("User connected", "system", "system"))
+    );
+
+    socket.on("chat message", function (message) {
+        message.owner = "other";
+        socket.broadcast.emit("chat message", message);
+    });
+
+    socket.on("disconnect", function () {
+        socket.emit("chat message",
+            (new Message("User disconnected", "system", "system")).toJson()
+        );
+        console.log("A user disconnected");
     });
 });
